d3b7224ef6eba3a762a30562976c0266702efba1
amies <amies@LAPTOP-VKVVDHFH>
2026-01-21 22:17:52 +0900

    Java導入

---
+++ helloworld_NakaoSeima/src/sample/SelfIntroduction.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// 氏名
+		String name ="中尾　晟麻";
+		// 年齢
+		int age = 23;
+		// 趣味
+		String hobby = "映画鑑賞";
+		
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
