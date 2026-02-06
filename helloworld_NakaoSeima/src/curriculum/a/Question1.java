4cba9fc5499a7b546566a6c3d492441d97598622
seima11 <seimasyd11@gmail.com>
2026-02-06 13:47:08 +0900

    Update Question1.java

--- helloworld_NakaoSeima/src/curriculum/a/Question1.java
+++ helloworld_NakaoSeima/src/curriculum/a/Question1.java
@@ -3,33 +3,24 @@ package curriculum.a;
 public class Question1 {
  public static void main(String[] args) {
  //Q1
-	 // 変数byteNumの初期値を設定
-	 byte byteNum  = 0;
-	 System.out.println(byteNum);
+	// 変数byteNumの初期値を設定
+	 byte byteNum;
 	 //変数shortNumの初期値を設定
-	 short shortNum  = 0;
-	 System.out.println(shortNum);
+	 short shortNum;
 	 //変数intNumの初期値を設定
-	 int intNum = 0;
-	 System.out.println(intNum);
+	 int intNum;
 	 //変数longNumの初期値を設定
-	 long longNum = 0;
-	 System.out.println(longNum);
+	 long longNum;
 	 //変数floatNumの初期値を設定
-	 float floatNum = 0;
-	 System.out.println(floatNum);
+	 float floatNum;
 	 //変数doubleNumの初期値を設定
-	 double doubleNum = 0;
-	 System.out.println(doubleNum);
+	 double doubleNum;
 	 //変数letterの初期値を設定
-	 char letter = 0;
-	 System.out.println(letter);
+	 char letter;
 	 //変数lettersの初期値を設定
-	 String letters = "a";
-	 System.out.println(letters);
+	 String letters;
 	 //変数isBooleanの初期値を設定
-	 boolean isBoolean = true;
-	 System.out.println(isBoolean);
+	 boolean isBoolean;
 	 
 	 
  }
