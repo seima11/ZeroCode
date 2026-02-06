9362c6e3f9a6dc8c32afba301d5a42b918417eec
amies <amies@LAPTOP-VKVVDHFH>
2026-02-06 12:24:51 +0900

    2-7変数1 Q1のみ

---
+++ helloworld_NakaoSeima/src/curriculum/a/Question1.java
@@ -0,0 +1,35 @@
+package curriculum.a;
+
+public class Question1 {
+ public static void main(String[] args) {
+ //Q1
+	 // 変数byteNumの初期値を設定
+	 byte byteNum  = 0;
+	 System.out.println(byteNum);
+	 //変数shortNumの初期値を設定
+	 short shortNum  = 0;
+	 System.out.println(shortNum);
+	 //変数intNumの初期値を設定
+	 int intNum = 0;
+	 System.out.println(intNum);
+	 //変数longNumの初期値を設定
+	 long longNum = 0;
+	 System.out.println(longNum);
+	 //変数floatNumの初期値を設定
+	 float floatNum = 0;
+	 System.out.println(floatNum);
+	 //変数doubleNumの初期値を設定
+	 double doubleNum = 0;
+	 System.out.println(doubleNum);
+	 //変数letterの初期値を設定
+	 char letter = 0;
+	 System.out.println(letter);
+	 //変数lettersの初期値を設定
+	 String letters = "a";
+	 //変数isBooleanの初期値を設定
+	 boolean isBoolean = true;
+	 
+	 
+ }
+}
+
