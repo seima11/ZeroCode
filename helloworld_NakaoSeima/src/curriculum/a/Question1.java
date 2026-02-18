9b2f2d0564137ebcba82193ceceb959ad76c0669
amies <amies@LAPTOP-VKVVDHFH>
2026-02-18 13:41:30 +0900

    Q1 Q2完了

--- helloworld_NakaoSeima/src/curriculum/a/Question1.java
+++ helloworld_NakaoSeima/src/curriculum/a/Question1.java
@@ -3,7 +3,7 @@ package curriculum.a;
 public class Question1 {
  public static void main(String[] args) {
  //Q1
-	// 変数byteNumの初期値を設定
+	 // 変数byteNumの初期値を設定
 	 byte byteNum;
 	 //変数shortNumの初期値を設定
 	 short shortNum;
@@ -22,7 +22,91 @@ public class Question1 {
 	 //変数isBooleanの初期値を設定
 	 boolean isBoolean;
 	 
+	 //Q2
+	 byteNum = 10; //byteNumに10を代入
+	 System.out.println(byteNum);
+	 shortNum = 100; //shortNumに100を代入
+	 System.out.println(shortNum);
+	 intNum = 1000; //intNumに1000を代入
+	 System.out.println(intNum);
+	 longNum = 10000; //longNumに10000を代入
+	 System.out.println(longNum);
+	 floatNum = 9.5f; //floatNumに9.5fを代入
+	 System.out.println(floatNum);
+	 doubleNum = 10.5; //doubleNumに10.5を代入
+	 System.out.println(doubleNum);
+	 letter = 'a'; //letterにaを代入
+	 System.out.println(letter);
+	 letters = "ハロー"; //lettersにハローを代入
+	 System.out.println(letters);
+	 isBoolean = true; //isBooleanにtrueを代入
+	 System.out.println(isBoolean);
 	 
- }
+	 //Q3
+	 //11110を出力
+	 System.out.println(byteNum + shortNum + intNum + longNum);
+	 //20を出力
+	 System.out.println(byteNum + byteNum);
+	 //a ハロー　trueを出力
+	 System.out.println(letter + letters + isBoolean);
+	 //11130を出力
+	 System.out.println(longNum + intNum + shortNum + byteNum + byteNum + byteNum);
+	 //10000000000を出力
+	 System.out.println(byteNum  * shortNum * intNum * longNum);
+	 //0.105を出力
+	 System.out.println(doubleNum / shortNum);
+	 //-90を出力
+	 System.out.println(byteNum - shortNum);
+	 
+	 //Q4
+	 String name = "山田太郎"; //nameに山田太郎を代入
+	 System.out.println("こんにちは、" + name + "さん！");
+	 
+	 //Q5
+	 int age = 25; //ageに25代入
+	 System.out.println("年齢:" + age + "歳");
+	 
+	 //Q6
+	 int num1 = 10; //num1に10代入
+	 int num2 = 5; //num2に5代入
+	 int sum = num1 + num2; //num1とnum2を足す
+	 System.out.println(sum);
+	 
+	 //Q7
+	 int score = 80; //scoreに80代入
+	 score = 20; //20に更新
+	 System.out.println(score + 80);
+	 
+	 //Q8
+	 double price = 99.99; //priceに99.99代入
+	 int i = (int)price; //double→intへ置き換え
+	 System.out.println(i);
+	 
+	 //Q9
+	 String numStr = "123"; //numStrに"123"代入
+	 int Num = Integer.parseInt(numStr); //numStrをintに変換
+	 System.out.println(Num + 10);
+	 
+	 //Q10
+	 int num =50; //numに50代入
+	 String str = String.valueOf(num); //numをStringに変換
+	 System.out.println("得点:" + str + "点");
+	 
+	 //Q11
+	 int a = 10; //aに10代入
+	 int b = 20; //bに20代入
+	 boolean result = (a < b); //a が b より小さいかどうか
+	 System.out.println(result);
+	 
+	 //Q12
+	 int x = 15; //xに15を代入
+	 String rs = (x >= 10) ? "OK" : "NG"; //x が 10 以上なら "OK"、そうでなければ "NG"
+	 System.out.println(rs);
+	 
+	 //Q13
+	 String text = "私はJavaが好きです。Javaは楽しい！";
+	 String Text = "私はPythonが好きです。Pythonは楽しい！"; //javaをPythonに変更
+	 System.out.println(Text);
+	 }
 }
 
---
+++ helloworld_NakaoSeima/src/curriculum/b/Question2.java
@@ -0,0 +1,129 @@
+package curriculum.b;
+
+import java.util.Scanner;
+public class Question2 {
+
+	public static void main(String[] args) {
+	
+//Q1
+	int score = 75; //int 型の変数 score に 75 を代入
+	if (score >= 60) { //score が 60 以上なら "合格です！" と表示
+		System.out.println("合格です！"); 
+	}
+//Q2	
+	int age = 25; // int 型の変数 age に 25 を代入
+	if (age >= 20 && age <= 30) { //age が 20 以上 30 以下なら "適正年齢です" と表示
+		System.out.println("適正年齢です");
+	} else { //それ以外の場合は "対象外です" と表示
+		System.out.println("対象外です");
+	}
+//Q3	
+	int age1 = 18;
+	if (age1 >= 20) {
+		System.out.println("成人です");
+	} else if(age1 >= 13 && age1 <= 19) {
+		System.out.println("ティーンエイジャーです");
+	} else {	
+		System.out.println("子供です");
+	}
+//Q4
+	int X = 30;
+	int Y = 15;
+	int Z = 50;
+	if (X >= Y && X >= Z) {
+		System.out.println(X);
+	}
+//Q5
+	Scanner sc = new Scanner(System.in);
+	int num = sc.nextInt();
+	if (num > 0) {
+		System.out.println("正の数です");
+	}else if (num == 0) {
+		System.out.println("0です");
+	}else {
+		System.out.println("負の数です");
+	}
+//Q6
+	Scanner sc1 = new Scanner(System.in);
+	int varue = sc1.nextInt();
+	if (varue % 2 == 0) {
+		System.out.println("偶数です");
+	} else {
+		System.out.println("奇数です");
+	}
+//Q7
+	Scanner sc2 = new Scanner(System.in);
+	int score1 = sc2.nextInt();
+	if (score1 >= 90) {
+		System.out.println("優");
+	} else if (score1 >= 70) {
+		System.out.println("良");
+	} else if (score1 >= 50) {
+	   System.out.println("可");
+	} else {
+		System.out.println("不可");
+	}
+//Q8
+	Scanner sc3 = new Scanner(System.in);
+	String inport = sc3.nextLine();
+	if (inport == null || inport.equals("")){
+		System.out.println("入力が無効です");
+	}
+//Q9
+	Scanner sc4 = new Scanner(System.in);
+	int day = sc4.nextInt();
+	switch (day) {
+	case 1:
+		System.out.println("月曜日");
+		break;
+	case 2:
+		System.out.println("火曜日");
+		break;
+	case 3:
+		System.out.println("水曜日");
+		break;
+	case 4:
+		System.out.println("木曜日");
+		break;
+	case 5:
+		System.out.println("金曜日");
+		break;
+    case 6:
+    	System.out.println("土曜日");
+    	break;
+	case 7:
+		System.out.println("日曜日");
+		break;
+	default:
+		System.out.println("無効な入力です");
+	}
+//Q10
+	Scanner sc5 = new Scanner(System.in);
+	int mouth = sc5.nextInt();
+	switch (mouth) {
+	    case 12:
+	    case 1:
+	    case 2:
+	    	System.out.println("冬");
+	    	break;
+	    case 3:
+	    case 4:
+	    case 5:
+	    	System.out.println("春");
+	    	break;
+	    case 6:
+	    case 7:
+	    case 8:
+	    	System.out.println("夏");
+	    	break;
+	    case 9:
+	    case 10:
+	    case 11:
+	    	System.out.println("秋");
+	    	break;
+	    default:
+	    	System.out.println("無効な月です");
+	}
+	sc.close();
+	}
+}
